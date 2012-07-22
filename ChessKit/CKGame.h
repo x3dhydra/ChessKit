97e96d453258b64a3bc9396e73d3856a70d89977
Austen Green <x3dhydra@gmail.com>
2012-07-22 18:54:16 -0500

    Added more metadata keys.

--- ChessKit/CKGame.h
+++ ChessKit/CKGame.h
@@ -10,11 +10,21 @@
 #import "CKPosition.h"
 #import "CKGameTree.h"
 
+typedef enum
+{
+    CKGameSevenTagRoster   = 0,
+    CKGamePlayerAttributes = 1 << 0,
+    CKGameEventAttributes  = 1 << 1,
+    CKGameAttributes       = 1 << 2,
+} CKGameAttributeMask;
+
 @interface CKGame : NSObject
 @property (nonatomic, strong) NSDictionary *metadata;
 
 + (id)game;
 + (id)gameWithStartingPosition:(CKPosition *)position;
+
++ (NSArray *)attributesForMask:(CKGameAttributeMask)mask;
 
 - (id)initWithStartingPosition:(CKPosition *)position;
 
@@ -32,9 +42,15 @@
 - (NSNumber *)blackELO;
 - (NSNumber *)round;
 
+- (NSDictionary *)metadataForKeys:(NSArray *)keys;
+
+- (NSString *)localizedAttributeForKey:(NSString *)key;
+
 @end
 
 // Game metadata attribute.  Used by CKFetchRequest for potentially optimized searches
+
+// Seven Tag Roster
 extern NSString *const CKGameEventKey;
 extern NSString *const CKGameSiteKey;
 extern NSString *const CKGameDateKey;
@@ -42,3 +58,19 @@ extern NSString *const CKGameRoundKey;
 extern NSString *const CKGameWhitePlayerKey;
 extern NSString *const CKGameBlackPlayerKey;
 extern NSString *const CKGameResultKey;
+
+// Player-related tags
+extern NSString * const CKGameWhiteTitleKey;
+extern NSString * const CKGameBlackTitleKey;
+extern NSString * const CKGameWhiteELOKey;
+extern NSString * const CKGameBlackELOKey;
+extern NSString * const CKGameWhiteUSCFKey;
+extern NSString * const CKGameBlackUSCFKey;
+
+// Event-related tags
+extern NSString * const CKGameEventDateKey;
+extern NSString * const CKGameEventSponsorKey;
+extern NSString * const CKGameStageKey;
+extern NSString * const CKGameBoardKey;
+extern NSString * const CKGameOpeningKey;
+extern NSString * const CKGameECOKey;
--- ChessKit/CKGame.m
+++ ChessKit/CKGame.m
@@ -70,6 +70,81 @@
     return _gameTree;
 }
 
+- (NSString *)localizedAttributeForKey:(NSString *)key
+{
+    return NSLocalizedStringFromTable(key, @"PGNTags", nil);
+}
+
+#pragma mark - Attributes
+
++ (NSArray *)attributesForMask:(CKGameAttributeMask)mask
+{
+    if (mask == CKGameSevenTagRoster)
+    {
+        return [NSArray arrayWithObjects:
+                CKGameEventKey, 
+                CKGameSiteKey, 
+                CKGameDateKey, 
+                CKGameRoundKey,
+                CKGameWhitePlayerKey,
+                CKGameBlackPlayerKey,
+                CKGameResultKey,
+                nil];
+    }
+    
+    NSMutableArray *attributes = [NSMutableArray array];
+    if (mask & CKGamePlayerAttributes)
+    {
+        [attributes addObjectsFromArray:
+         [NSArray arrayWithObjects:
+          CKGameWhitePlayerKey,
+          CKGameWhiteELOKey,
+          CKGameWhiteTitleKey,
+          CKGameWhiteUSCFKey,
+          CKGameBlackPlayerKey,
+          CKGameBlackELOKey,
+          CKGameBlackTitleKey,
+          CKGameBlackUSCFKey,
+          nil]];
+    }
+    if (mask & CKGameEventAttributes)
+    {
+        [attributes addObjectsFromArray:
+         [NSArray arrayWithObjects:
+          CKGameEventKey,
+          CKGameSiteKey,
+          CKGameEventDateKey,
+          CKGameEventSponsorKey,
+          CKGameStageKey,
+          nil]];
+    }
+    if (mask & CKGameAttributes)
+    {
+        [attributes addObjectsFromArray:
+         [NSArray arrayWithObjects:
+          CKGameRoundKey,
+          CKGameResultKey,
+          CKGameECOKey,
+          CKGameOpeningKey,
+          CKGameBoardKey,
+          nil]];
+    }
+    
+    return attributes;
+}
+
+- (NSDictionary *)metadataForKeys:(NSArray *)keys
+{
+    NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+    [keys enumerateObjectsUsingBlock:^(NSString * key, NSUInteger idx, BOOL *stop) {
+        id value = [self.metadata objectForKey:key];
+        if (value)
+            [dictionary setObject:value forKey:key];
+    }];
+    
+    return dictionary;
+}
+
 @end
 
 NSString *const CKGameEventKey = @"Event";
@@ -79,3 +154,17 @@ NSString *const CKGameRoundKey = @"Round";
 NSString *const CKGameWhitePlayerKey = @"White";
 NSString *const CKGameBlackPlayerKey = @"Black";
 NSString *const CKGameResultKey = @"Result";
+
+NSString * const CKGameWhiteTitleKey = @"WhiteTitle";
+NSString * const CKGameBlackTitleKey = @"BlackTitle";
+NSString * const CKGameWhiteELOKey = @"WhiteElo";
+NSString * const CKGameBlackELOKey = @"BlackElo";
+NSString * const CKGameWhiteUSCFKey = @"WhiteUSCF";
+NSString * const CKGameBlackUSCFKey = @"BlackUSCF";
+
+NSString * const CKGameEventDateKey = @"EventDate";
+NSString * const CKGameEventSponsorKey = @"EventSponsor";
+NSString * const CKGameStageKey = @"Stage";
+NSString * const CKGameBoardKey = @"Board";
+NSString * const CKGameOpeningKey = @"Opening";
+NSString * const CKGameECOKey = @"ECO";
